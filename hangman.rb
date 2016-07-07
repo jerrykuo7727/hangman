d8a2c9759b6edb1ffe2926ad21b991a7bc478442
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-07-08 05:02:04 +0800

    Add function to guess answer

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,5 @@
 class Hangman
-  attr_reader :answer
+  attr_reader :answer, :guesses_left
   def initialize
     @answer = random_word
     @guesses_left = 6
@@ -10,15 +10,35 @@ class Hangman
     @guess.join(' ')
   end
 
+  def try_to_guess(input)
+    guess(input) if valid_guess?(input)
+  end
+
   private
 
   def random_word
     File.open('dictionary.txt', 'r') do |file|
-      file.readlines.select{ |word| word.length.between?(6, 13) }.sample.chomp
+      file.readlines.select{ |word| word.length.between?(6, 13) }.sample.chomp.split('')
     end
+  end
+
+  def valid_guess?(input)
+    input.length == 1 && /[A-Za-z]/ === input
+  end
+
+  def guess(input)
+    guess_right = false
+    @answer.each_with_index do |word, i|
+      if word.downcase == input
+        guess_right = true
+        @guess[i] = word
+      end
+    end
+    @guesses_left -= 1 unless guess_right
   end
 end
 
 hangman = Hangman.new
 p hangman.answer
 p hangman.display
+p hangman.guesses_left
